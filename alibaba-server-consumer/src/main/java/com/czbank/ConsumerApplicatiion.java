416f3fada0990fed3be95d979915d3ffb426e995
foreverActiveBoy <extends_zkx0924@163.com>
2020-04-14 21:34:54 +0800

    v1.0.0-spring-cloud-alibaba 项目骨架

---
+++ alibaba-server-consumer/src/main/java/com/czbank/ConsumerApplicatiion.java
@@ -0,0 +1,17 @@
+package com.czbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author foreverActiveBoy
+ * @EnableDiscoveryClient 开启向注册中心注册服务
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerApplicatiion {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplicatiion.class,args);
+    }
+}
