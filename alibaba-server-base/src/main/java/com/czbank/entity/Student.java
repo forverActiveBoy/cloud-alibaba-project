aee43de4437318dfcf726b2b1db8f690bc6a324f
foreverActiveBoy <extends_zkx0924@163.com>
2020-04-21 17:04:11 +0800

    v1.0.0-base层-对实体增加JSR303注解

--- alibaba-server-base/src/main/java/com/czbank/entity/Student.java
+++ alibaba-server-base/src/main/java/com/czbank/entity/Student.java
@@ -3,6 +3,10 @@ package com.czbank.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
@@ -18,11 +22,13 @@ public class Student implements Serializable {
     private static final long serialVersionUID = 769744065415731630L;
     
     private Long id;
-    
+
+    @NotBlank(message = "名字不能为空！")
     private String name;
-    
+
     private BigDecimal score;
-    
+
+    @Email(message = "邮箱不符合规则！")
     private String email;
 
 }
