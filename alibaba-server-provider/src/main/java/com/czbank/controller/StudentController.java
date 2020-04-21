7d6766d0e5b6c727f56f63425569cea7674e0b00
foreverActiveBoy <extends_zkx0924@163.com>
2020-04-21 17:04:35 +0800

    v1.0.0-provider层-student控制层

--- alibaba-server-provider/src/main/java/com/czbank/controller/StudentController.java
+++ alibaba-server-provider/src/main/java/com/czbank/controller/StudentController.java
@@ -2,18 +2,22 @@ package com.czbank.controller;
 
 import com.czbank.entity.Student;
 import com.czbank.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
  * @author foreverActiveBoy
+ * 备注:使用rest风格
  */
-
 @RestController
+@Slf4j
 public class StudentController {
     @Autowired
     private StudentService studentService;
@@ -21,5 +25,11 @@ public class StudentController {
     @RequestMapping(value = {"/v1/student"},method = {RequestMethod.GET})
     public List<Student> queryList(){
         return studentService.queryList();
+    }
+
+    @RequestMapping(value = {"/v1/student"},method = {RequestMethod.POST})
+    public void addOne(@Valid @RequestBody Student student){
+        log.info("添加学生对象入参student:[{}]",student);
+        studentService.addStudent(student);
     }
 }
