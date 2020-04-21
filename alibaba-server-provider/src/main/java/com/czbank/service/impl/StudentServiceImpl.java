54a6a0e430f7f3c5a2e828578b0e69d1cc682216
foreverActiveBoy <extends_zkx0924@163.com>
2020-04-21 12:52:52 +0800

    v1.0.0-provider层-student接口实现类

---
+++ alibaba-server-provider/src/main/java/com/czbank/service/impl/StudentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.czbank.service.impl;
+
+import com.czbank.dao.StudentDao;
+import com.czbank.entity.Student;
+import com.czbank.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author foreverActiveBoy
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentDao studentDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Student> queryList() {
+        return studentDao.queryAll(null);
+    }
+}
