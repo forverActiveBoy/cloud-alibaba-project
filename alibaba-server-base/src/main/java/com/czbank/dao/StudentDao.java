35a8161e4a40ee624caedbdbd356259f444c68c9
foreverActiveBoy <extends_zkx0924@163.com>
2020-04-21 12:51:18 +0800

    v1.0.0-base层-studentDao接口

---
+++ alibaba-server-base/src/main/java/com/czbank/dao/StudentDao.java
@@ -0,0 +1,65 @@
+package com.czbank.dao;
+
+import com.czbank.entity.Student;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+
+/**
+ * (Student)表数据库访问层
+ *
+ * @author makejava
+ * @since 2020-04-21 11:55:47
+ */
+public interface StudentDao {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    Student queryById(Long id);
+
+    /**
+     * 查询指定行数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    List<Student> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
+
+    /**
+     * 通过实体作为筛选条件查询
+     *
+     * @param student 实例对象
+     * @return 对象列表
+     */
+    List<Student> queryAll(Student student);
+
+    /**
+     * 新增数据
+     *
+     * @param student 实例对象
+     * @return 影响行数
+     */
+    int insert(Student student);
+
+    /**
+     * 修改数据
+     *
+     * @param student 实例对象
+     * @return 影响行数
+     */
+    int update(Student student);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 影响行数
+     */
+    int deleteById(Long id);
+
+}
